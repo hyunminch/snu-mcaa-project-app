67eebeddbd7a40112130aec4b6feb0adfaf6b545
CuriousLocky <835322750jm37@outlook.com>
2019-11-03 21:02:21 +0900

    Update profile tab so it works less poorly on every tested device

--- app/src/main/java/io/github/snumcaa/MainActivity.java
+++ app/src/main/java/io/github/snumcaa/MainActivity.java
@@ -1,6 +1,8 @@
 package io.github.snumcaa;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -24,5 +26,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+
+    public void profileClicked(View view){
+        Intent intent = new Intent(MainActivity.this, Profile_ProfileActivity.class);
+        startActivity(intent);
     }
 }
