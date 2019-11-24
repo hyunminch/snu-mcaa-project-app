b10567261d6b6a3b0b09fea85d908dcab138f5e7
Hyun Min Choi <hyunmin.personal@gmail.com>
2019-11-24 16:43:32 +0900

    Cleanup ProfileActivity and SettingActivity

--- app/src/main/java/io/github/snumcaa/ProfileActivity.java
+++ app/src/main/java/io/github/snumcaa/ProfileActivity.java
@@ -1,24 +1,12 @@
 package io.github.snumcaa;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.PopupWindow;
-import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import com.r0adkll.slidr.Slidr;
 
-import org.w3c.dom.Text;
-
 public class ProfileActivity extends FragmentActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
