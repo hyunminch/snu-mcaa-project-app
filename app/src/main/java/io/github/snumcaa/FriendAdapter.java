16501c2386465fee32437121a1a673a68a0522f4
CuriousLocky <835322750jm37@outlook.com>
2019-12-06 19:55:58 +0900

    partially implement adapter

--- app/src/main/java/io/github/snumcaa/FriendAdapter.java
+++ app/src/main/java/io/github/snumcaa/FriendAdapter.java
@@ -1,6 +1,37 @@
 package io.github.snumcaa;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class FriendAdapter extends BaseAdapter {
+    private ArrayList<Search_result> data;
+    private Context context;
+    FriendAdapter(Context context, Collection<Search_result> data){
+        this.data = new ArrayList<Search_result>(data);
+        this.context = context;
+    }
+    @Override
+    public int getCount(){
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position){
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position){
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        return null;
+    }
 }
